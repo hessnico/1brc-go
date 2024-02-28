6cb6fb4b8630d0ff9f3fc1389702565123887436
Nicolas <nicolashess1@gmail.com>
2024-02-28 17:43:10 -0300

    run bash script

---
+++ run.sh
@@ -0,0 +1,9 @@
+go run main.go ./first_implementation.go;
+sleep 30;
+go run main.go ./first_implementation.go;
+sleep 30;
+go run main.go ./first_implementation.go;
+sleep 30;
+go run main.go ./first_implementation.go;
+sleep 30;
+go run main.go ./first_implementation.go;
